65fc69cd0a245ee048a9d8cb42653acf1013c66d
Adrian Kuśmierek <51207265+AdrianKusmierek@users.noreply.github.com>
2019-06-26 21:04:30 +0200

    Add files via upload

---
+++ commands/leave.js
@@ -0,0 +1,19 @@
+exports.run = (client, message, args, ops) => {
+
+    if (!message.member.voiceChannel) return message.channel.send('1');
+
+    if (!message.member.voiceChannel) return message.channel.send('2');
+
+    if (message.guild.me.voiceChannelID !== message.member.voiceChannelID) return message.channel.send('3');
+
+    message.guild.me.voiceChannel.leave();
+
+    message.channel.send('Leaving Channel...')
+	
+	message.delete({timeout: 1000});
+	
+}
+
+exports.config = {
+	command: 'leave'
+}
