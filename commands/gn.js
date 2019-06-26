e3c17a79669fa7f158a4cd986498ff1743088538
Adrian Kuśmierek <51207265+AdrianKusmierek@users.noreply.github.com>
2019-06-26 22:49:40 +0200

    Update gn.js

--- commands/gn.js
+++ commands/gn.js
@@ -1,6 +1,6 @@
 module.exports.run = async (client, message, args) => {
 	
-	message.channel.send('Good Night!! http://www.sarkarinaukrisearch.in/wp-content/uploads/2019/01/good-night-pictures-hd-download-14.jpg')
+	message.channel.send('Good Night **' + message.member.user.tag + '**!! http://www.sarkarinaukrisearch.in/wp-content/uploads/2019/01/good-night-pictures-hd-download-14.jpg')
 	message.delete({timeout: 1000});
 	
 }
