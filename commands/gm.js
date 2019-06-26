61c2c69817dad3a5805c76e03ec4730f66b7b68c
Adrian Kuśmierek <51207265+AdrianKusmierek@users.noreply.github.com>
2019-06-26 22:49:08 +0200

    Update gm.js

--- commands/gm.js
+++ commands/gm.js
@@ -1,6 +1,6 @@
 module.exports.run = async (client, message, args) => {
 	
-        message.channel.send('Good Morning!! https://img.etimg.com/thumb/msid-67055775,width-643,imgsize-709079,resizemode-4/coffeebeans.jpg')
+        message.channel.send('Good Morning **' + message.member.user.tag + '**!!https://img.etimg.com/thumb/msid-67055775,width-643,imgsize-709079,resizemode-4/coffeebeans.jpg')
 		message.delete({timeout: 1000});
 }
 
