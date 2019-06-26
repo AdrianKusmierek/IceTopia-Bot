524cb9231bf70b435653189d99adbd304365d595
Adrian Kuśmierek <51207265+AdrianKusmierek@users.noreply.github.com>
2019-06-26 21:31:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -138,6 +138,18 @@ if (msg.startsWith(prefix + 'EVENT')) {
      message.delete({timeout: 1000});	
 }
 	
+if (msg.startsWith(prefix + 'MELD')) {
+	       const color = args[0]
+           const text = args.slice(0).join(" ");
+           if (text.length < 1) return message.channel.send("Can not make an event of nothing");
+           const embed = new Discord.RichEmbed()
+           .setColor(0x0000ff)
+           .setTitle("Melding")
+           .setDescription(text);
+           message.channel.send({embed})
+     message.delete({timeout: 1000});	
+}
+	
    
    
    if (msg.startsWith(prefix + 'CLEAR')) {
