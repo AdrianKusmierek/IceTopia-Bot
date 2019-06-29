f6efa9bebb2d56ac93a4cd106f0549c5d61e6004
Adrian Kuśmierek <51207265+AdrianKusmierek@users.noreply.github.com>
2019-06-29 12:50:26 +0200

    Update ip.js

--- commands/ip.js
+++ commands/ip.js
@@ -1,6 +1,6 @@
 module.exprots.run = async (client, message, args) => {
 
-message.channel.send(IP: **145.239.98.210:25566**.)
+message.channel.send('IP: **145.239.98.210:25566**.')
 }
 
 module.exports.config = {
