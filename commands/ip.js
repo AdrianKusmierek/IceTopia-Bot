d60adf10c6bcbdc6c7731d21249050db10bd0ea3
Adrian Kuśmierek <51207265+AdrianKusmierek@users.noreply.github.com>
2019-06-29 12:54:58 +0200

    Delete ip.js

--- commands/ip.js
+++
@@ -1,9 +0,0 @@
-module.exprots.run = async (client, message, args) => {
-
-        message.channel.send('IP: **145.239.98.210:25566**.')
-                message.delete({timeout: 1000});
-}
-
-module.exports.config = {
-        command: 'ip'
-}
