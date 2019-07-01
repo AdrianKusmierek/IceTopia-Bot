c0d9e2d61d5f6101ce39b038a863ac23e08cae46
Adrian Kuśmierek <51207265+AdrianKusmierek@users.noreply.github.com>
2019-07-01 16:17:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -85,7 +85,7 @@ client.on('message', message => {
     }
     
     if (command === 'gm') {
-	    message.channel.send('Good Morning**' + message.member.user.tag + '**!!https://img.etimg.com/thumb/msid-67055775,width-643,imgsize-709079,resizemode-4/coffeebeans.jpg');
+	    message.channel.send('Good Morning **' + message.member.user.tag + '** !!https://img.etimg.com/thumb/msid-67055775,width-643,imgsize-709079,resizemode-4/coffeebeans.jpg');
 	    message.delete({timeout: 1000});
     }
 
