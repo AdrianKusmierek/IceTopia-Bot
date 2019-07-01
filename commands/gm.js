16b4f6b0b3cd107c0d7b78458b96fd18bce656dd
Adrian Kuśmierek <51207265+AdrianKusmierek@users.noreply.github.com>
2019-07-01 16:23:25 +0200

    Update gm.js

--- commands/gm.js
+++ commands/gm.js
@@ -2,7 +2,6 @@ module.exports = {
         name: '\gm',
         description: 'Good Morning** message.member.user.tag **!!https://img.etimg.com/thumb/msid-67055775,width-643,imgsize-709079,resizemode-4/coffeebeans.jp',
         execute(message, args) {
-                message.channel.send('Good Morning **' + message.member.user.tag + '** !!https://img.etimg.com/thumb/msid-67055775,width-643,imgsize-709079,resizemode-4/coffeebeans.jpg');
-                message.delete({timeout: 1000});
+                
         },
 };
