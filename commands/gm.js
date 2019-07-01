5dbc6c4938739fbbcb9e086a5a0d6486a60ac5c7
Adrian Kuśmierek <51207265+AdrianKusmierek@users.noreply.github.com>
2019-07-01 16:10:11 +0200

    Delete gm.js

--- commands/gm.js
+++
@@ -1,9 +0,0 @@
-module.exports.run = async (client, message, args) => {
-	
-        message.channel.send('Good Morning **' + message.member.user.tag + '**!!https://img.etimg.com/thumb/msid-67055775,width-643,imgsize-709079,resizemode-4/coffeebeans.jpg')
-		message.delete({timeout: 1000});
-}
-
-module.exports.config = {
-	command: 'gm'
-}
