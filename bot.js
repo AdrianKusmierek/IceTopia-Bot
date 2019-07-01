90cc77b237e9567afb5faf4b0eefd2619eab9ba9
Adrian Kuśmierek <51207265+AdrianKusmierek@users.noreply.github.com>
2019-07-01 23:37:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 // Calling the package
 const Discord = require("discord.js")
-const client = new Discord.Client
+const client = new Discord.Client﻿
 const ownerID = '501122648806260740';
 const fs = require('fs');
 const prefix = '\\';
@@ -9,6 +9,12 @@ const prefix = '\\';
 
 const commandsList = (fs.readFileSync('Storage/commands.txt', 'utf8'));
 client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 // Functions
 function userInfo(user) {
